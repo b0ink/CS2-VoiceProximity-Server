da9d647b159ec7ce0ba86988d460b8365e5c584e
b0ink <40929320+b0ink@users.noreply.github.com>
2025-04-19 19:07:21 +1000

    chore: use env variables

--- index.ts
+++ index.ts
@@ -1,138 +1,157 @@
-const express = require("express");
-const { Server } = require("socket.io");
-const app = express();
+import { Server } from 'socket.io';
+import http from 'http';
+import mysql from 'mysql2';
 
-const io = new Server(3000, { cors: { origin: "*" } });
+const isProduction = process.env.NODE_ENV === 'production';
+const port = isProduction ? 80 : 3000;
+const domain = process.env.DOMAIN_URL || 'localhost';
 
-const config = require("./db.json");
+const server = http.createServer();
 
-const mysql = require("mysql2");
+const io = new Server(server, {
+  cors: {
+    origin: isProduction ? domain : '*', // Replace with your frontend domain in production
+    methods: ['GET', 'POST'],
+  },
+});
 
+// TODO: pull db information from the room host
+const config = {
+  DatabaseHost: process.env.DATABASE_HOST,
+  DatabaseUser: process.env.DATABASE_USER,
+  DatabasePassword: process.env.DATABASE_PASSWORD,
+  DatabaseName: process.env.DATABASE_NAME,
+};
+
+const databaseFetchRate: number = Number(process.env.DATABASE_FETCH_RATE) || 50;
 let connection: any;
 
 const setDbConnection = () => {
-    connection = mysql.createPool({
-        host: config.DatabaseHost,
-        user: config.DatabaseUser,
-        password: config.DatabasePassword,
-        database: config.DatabaseName,
-    });
+  connection = mysql.createPool({
+    host: config.DatabaseHost,
+    user: config.DatabaseUser,
+    password: config.DatabasePassword,
+    database: config.DatabaseName,
+  });
 };
 
 // Query the database and send the results to connected users
 const queryDatabaseAndUpdatePlayers = () => {
-    if (!connection || connection.state == "disconnected") {
-        console.log("not connected");
-        return;
+  if (!connection || connection.state == 'disconnected') {
+    console.log('not connected');
+    return;
+  }
+  connection.query('SELECT * FROM ProximityData', (err: any, results: any) => {
+    if (err) {
+      console.error('Error fetching player positions:', err);
+      return;
     }
-    connection.query("SELECT * FROM ProximityData", (err: any, results: any) => {
-        if (err) {
-            console.error("Error fetching player positions:", err);
-            return;
-        }
 
-        // console.log(results)
+    // console.log(results)
 
-        // Send player position data to all connected clients
-        io.emit("player-positions", results);
-    });
+    // Send player position data to all connected clients
+    io.emit('player-positions', results);
+  });
 };
 
-// Run the database query every 0.1 seconds (100ms)
-setInterval(queryDatabaseAndUpdatePlayers, 50);
+setInterval(queryDatabaseAndUpdatePlayers, databaseFetchRate);
+
 class JoinedPlayers {
-    // TODO; is there any persistence for socketIds?? when page is refreshed, new id is generated.
-    socketId = null;
-    steamId = null;
+  // TODO; is there any persistence for socketIds?? when page is refreshed, new id is generated.
+  socketId = null;
+  steamId = null;
 }
 
 class RoomData {
-    roomCode_ = null;
-    maxPlayers_ = 10;
-    joinedPlayers = [];
+  roomCode_ = null;
+  maxPlayers_ = 10;
+  joinedPlayers = [];
 
-    constructor(roomCode: any, maxPlayers?: any) {
-        this.roomCode_ = roomCode;
-        this.maxPlayers_ = maxPlayers;
-    }
+  constructor(roomCode: any, maxPlayers?: any) {
+    this.roomCode_ = roomCode;
+    this.maxPlayers_ = maxPlayers;
+  }
 }
 
-const rooms: any[] = [new RoomData("123")];
+const rooms: any[] = [new RoomData('123')];
 
 // interface JoinRoomData{
 //   roomCode?: String;
 //   steamId?: String;
 // }
 
-io.on("connection", (socket: any) => {
-    console.log("New user connected: ", socket.id);
+io.on('connection', (socket: any) => {
+  console.log('New user connected: ', socket.id);
 
-    // Handle joining a room
-    // TODO: steamId and clientId are the same right now
-    socket.on("join-room", (roomCode: any, steamId: any, clientId: any, isHost: any) => {
-        //TODO; capacity limits on joining room
-        //TODO: to "create a room", lobby host will need to pass in the database connection.
-        //TODO: if the db connection fails (doesnt find a table), it won't create new room
+  // Handle joining a room
+  // TODO: steamId and clientId are the same right now
+  socket.on('join-room', (roomCode: any, steamId: any, clientId: any, isHost: any) => {
+    //TODO; capacity limits on joining room
+    //TODO: to "create a room", lobby host will need to pass in the database connection.
+    //TODO: if the db connection fails (doesnt find a table), it won't create new room
 
-        console.log(`joinRoom called with ${roomCode}, ${steamId}`);
-        if (!roomCode) return; // If no room is provided, ignore the join attempt.
+    console.log(`joinRoom called with ${roomCode}, ${steamId}`);
+    if (!roomCode) return; // If no room is provided, ignore the join attempt.
 
-        let roomExists = false;
-        for (const roomData of rooms) {
-            if (roomData.roomCode_ === roomCode) {
-                roomExists = true;
-            }
-        }
+    let roomExists = false;
+    for (const roomData of rooms) {
+      if (roomData.roomCode_ === roomCode) {
+        roomExists = true;
+      }
+    }
 
-        if (!roomExists) {
-            // TODO: "reject" the attempt so that the user is notified this room doesnt exist
-            // callback({ error: "Room doesn't exist" });
-            console.log("room doesnt exist, notify the user to try again!");
-            return;
-        }
+    if (!roomExists) {
+      // TODO: "reject" the attempt so that the user is notified this room doesnt exist
+      // callback({ error: "Room doesn't exist" });
+      console.log('room doesnt exist, notify the user to try again!');
+      return;
+    }
 
-        let newPlayer = new JoinedPlayers();
-        newPlayer.socketId = socket.id;
-        newPlayer.steamId = steamId;
-        rooms[0].joinedPlayers.push(newPlayer);
+    let newPlayer = new JoinedPlayers();
+    newPlayer.socketId = socket.id;
+    newPlayer.steamId = steamId;
+    rooms[0].joinedPlayers.push(newPlayer);
 
-        socket.join(roomCode);
-        console.log(`${socket.id} joined room: ${roomCode} with steamid ${steamId}`);
+    socket.join(roomCode);
+    console.log(`${socket.id} joined room: ${roomCode} with steamid ${steamId}`);
 
-        // callback({ success: "joined room" });
+    // callback({ success: "joined room" });
 
+    // Notify other users in the room about the new user joining
+    console.log(
+      `calling user-joined with ${socket.id} ${JSON.stringify({
+        steamId: steamId,
+        clientId: steamId,
+      })}`
+    );
+    socket.to(roomCode).emit('user-joined', socket.id, { steamId: steamId, clientId: steamId });
 
-        // Notify other users in the room about the new user joining
-        console.log(
-            `calling user-joined with ${socket.id} ${JSON.stringify({
-                steamId: steamId,
-                clientId: steamId,
-            })}`
-        );
-        socket.to(roomCode).emit("user-joined", socket.id, { steamId: steamId, clientId: steamId });
-
-        // Handle signaling (peer-to-peer connections)
-        socket.on("signal", ({ to, data }: any) => {
-            io.to(to).emit("signal", {
-                from: socket.id,
-                data,
-                client: { steamId: steamId, clientId: steamId },
-            });
-        });
-
-        //TODO: handle a manual 'leave' event, removing their data from their joined room
-
-        // Handle user disconnection
-        socket.on("disconnect", () => {
-            //TODO: remove them from the joinedPLayers array
-            console.log(`${socket.id} disconnected`);
-            socket.to(roomCode).emit("user-left", socket.id);
-            socket.leave(roomCode); // Remove user from the room when they disconnect
-        });
+    // Handle signaling (peer-to-peer connections)
+    socket.on('signal', ({ to, data }: any) => {
+      io.to(to).emit('signal', {
+        from: socket.id,
+        data,
+        client: { steamId: steamId, clientId: steamId },
+      });
     });
+
+    //TODO: handle a manual 'leave' event, removing their data from their joined room
+
+    // Handle user disconnection
+    socket.on('disconnect', () => {
+      //TODO: remove them from the joinedPLayers array
+      console.log(`${socket.id} disconnected`);
+      socket.to(roomCode).emit('user-left', socket.id);
+      socket.leave(roomCode); // Remove user from the room when they disconnect
+    });
+  });
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-    setDbConnection();
+server.listen(port, () => {
+  if (!isProduction) {
+    console.log(`Server running on http://localhost:${port}`);
+  } else {
+    console.log(`Running server on `);
+  }
+  setDbConnection();
 });
