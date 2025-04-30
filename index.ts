d3368fc6502f7d34b9991b9cdd3ba056fd7a4def
b0ink <40929320+b0ink@users.noreply.github.com>
2025-05-01 01:27:09 +1000

    fix: pass through client object in user-left

--- index.ts
+++ index.ts
@@ -192,12 +192,12 @@ io.on('connection', (socket: any) => {
     // Handle user disconnection
     socket.on('disconnect', () => {
       //TODO: remove them from the joinedPLayers array
-      console.log(`${socket.id} disconnected. Cleaning up..`);
       // Evict player object
       for (const room of rooms) {
         room.joinedPlayers = room.joinedPlayers.filter((player) => player.socketId !== socket.id);
       }
-      socket.to(roomCode).emit('user-left', socket.id);
+      console.log(`${socket.id} disconnected. Cleaning up.. ${steamId}`);
+      socket.to(roomCode).emit('user-left', socket.id, { steamId: steamId, clientId: steamId });
       socket.leave(roomCode); // Remove user from the room when they disconnect
     });
   });
