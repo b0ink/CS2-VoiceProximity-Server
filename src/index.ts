94215ec288eef41d37e64aadc49ee59c9c168f81
b0ink <40929320+b0ink@users.noreply.github.com>
2025-05-06 07:24:48 +1000

    choe: debug address

--- src/index.ts
+++ src/index.ts
@@ -42,13 +42,13 @@ const testDefaultRoom = '123';
 const rooms: RoomData[] = [new RoomData(testDefaultRoom)];
 
 io.on('connection', (socket: Socket) => {
-  const port = socket.request.socket.remotePort;
+  const address = socket.request.socket.remoteAddress;
   const ip = socket.handshake.headers['x-forwarded-for'];
   const ip2 = socket.handshake.address; //?.split(',')[0]?.trim();
   const ua = socket.handshake.headers['user-agent'];
   const lang = socket.handshake.headers['accept-language'];
 
-  console.log('New connection from Port:', port);
+  console.log('New connection from address:', address);
   console.log('New connection from IP:', ip);
   console.log('New connection from IP2:', ip2);
   console.log('User-Agent:', ua);
