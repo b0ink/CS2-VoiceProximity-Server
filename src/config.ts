a3547128107db71aa1ef3e68115af6241f634f39
b0ink <40929320+b0ink@users.noreply.github.com>
2025-05-22 11:09:01 +1000

    chore: format

--- src/config.ts
+++ src/config.ts
@@ -3,28 +3,29 @@ import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+if (process.env.COTURN_STATIC_AUTH_SECRET === null) {
+  throw Error('Invalid or no COTURN_STATIC_AUTH_SECRET provided in environment variables.');
+}
+
+if (process.env.JWT_SECRET_KEY === null) {
+  throw Error('Invalid or no JWT_SECRET_KEY provided in environment variables.');
+}
+
+if (process.env.DEFAULT_SOCKET_API_KEY === null) {
+  throw Error('Invalid or no DEFAULT_SOCKET_API_KEY provided in environment variables.');
+}
+
 const isProduction = process.env.NODE_ENV === 'production';
 const DEBUG = process.env.DEBUG || false;
 const port = Number(process.env.PORT) || 3000;
 const domain = process.env.DOMAIN_URL || 'localhost';
 
-if (process.env.COTURN_STATIC_AUTH_SECRET === null) {
-  throw Error('Invalid or no COTURN_STATIC_AUTH_SECRET provided in environment variables.');
-}
 const coturnStaticAuthSecret = process.env.COTURN_STATIC_AUTH_SECRET as string;
-
 const coturnCredentialsExpiry = process.env.COTURN_CREDENTIALS_EXPIRY
   ? parseInt(process.env.COTURN_CREDENTIALS_EXPIRY)
   : 24 * 3600; // 24 hours
 
-if (process.env.JWT_SECRET_KEY === null) {
-  throw Error('Invalid or no JWT_SECRET_KEY provided in environment variables.');
-}
 const jwtSecretKey = process.env.JWT_SECRET_KEY as string;
-
-if (process.env.DEFAULT_SOCKET_API_KEY === null) {
-  throw Error('Invalid or no DEFAULT_SOCKET_API_KEY provided in environment variables.');
-}
 const defaultApiKey = process.env.DEFAULT_SOCKET_API_KEY as string;
 
 export {
